6ac1af0fa96e009299b6de5e864c6f4d4d5f12a3
yurchenkonatalya <70762884+yurchenkonatalya@users.noreply.github.com>
2021-12-27 22:59:02 +0300

    Add files via upload

---
+++ SPP_3/SPP_3/Mutex.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace SPP_3
+{
+    class Mutex
+    {
+        private int sem;
+
+        public Mutex()
+        {
+            sem = 0;
+        }
+
+        public void Lock()
+        {
+            while (Interlocked.CompareExchange(ref sem, Thread.CurrentThread.ManagedThreadId, 0) != 0)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        public void Unlock()
+        {
+            Interlocked.CompareExchange(ref sem, 0, Thread.CurrentThread.ManagedThreadId);
+        }
+
+    }
+}
---
+++ SPP_3/SPP_3/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SPP_3
+{
+    class Program
+    {
+        private static Mutex mutex = new Mutex();
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                Thread thread = new Thread(print);
+                thread.Start();
+            }
+            Console.ReadLine();
+         }
+
+    public static void print()
+    {
+        mutex.Lock();
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("thread " + Thread.CurrentThread.ManagedThreadId + " step " + i);
+        }
+        mutex.Unlock();
+    }
+}
+}
