9bdaaf65dec948db62c3aba9ee70989f882e0541
yurchenkonatalya <70762884+yurchenkonatalya@users.noreply.github.com>
2021-12-27 23:02:59 +0300

    Add files via upload

---
+++ SPP_9/SPP_9/DynamicList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace SPP_9
+{
+    public class DynamicList<T> : IEnumerable<T>
+    {
+        public int Count { private set; get; }
+
+        public T[] Items;
+
+        public DynamicList()
+        {
+            Items = Array.Empty<T>();
+        }
+
+        public void Add(T element)
+        {
+            T[] temp = new T[Count + 1];
+            Array.Copy(Items, temp, Items.Length);
+            temp[Count] = element;
+            Items = temp;
+            Count++;
+        }
+
+        public void Remove(T element)
+        {
+            Remove(Array.IndexOf(Items, element));
+        }
+
+        public void Remove(int index)
+        {
+            Array.Copy(Items, index + 1, Items, index, Count - index - 1);
+            Count--;
+            Items[Count] = default;
+        }
+
+        public void Clear()
+        {
+            Items = Array.Empty<T>();
+            Count = 0;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (var i = 0; i < Count; i++)
+            {
+                yield return Items[i];
+            }
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ SPP_9/SPP_9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SPP_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicList<int> list = new DynamicList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Remove(5);
+
+            IEnumerator<int> enumerator = list.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+            enumerator.Dispose();
+        }
+    }
+}
