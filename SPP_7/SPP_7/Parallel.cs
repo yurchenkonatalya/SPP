d47d6cf1e9d0b1cbce0f9a9f2514d39549da0ca2
yurchenkonatalya <70762884+yurchenkonatalya@users.noreply.github.com>
2021-12-27 23:01:47 +0300

    Add files via upload

---
+++ SPP_7/SPP_7/Parallel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+namespace SPP_7
+{
+    class Parallel
+    {
+        static CountdownEvent countOfActions;
+        public static void WaitAll(Task[] Tasks)
+        {
+            countOfActions = new CountdownEvent(Tasks.Length);
+            foreach (Task task in Tasks)
+            {
+                ThreadPool.QueueUserWorkItem(Execute, task);
+            }
+            countOfActions.Wait();
+        }
+
+        private static void Execute(object task)
+        {
+            Task method = (Task)task;
+            method.Invoke();
+            countOfActions.Signal();
+        }
+    }
+}
---
+++ SPP_7/SPP_7/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+namespace SPP_7
+{
+    delegate void Task();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task[] Tasks = new Task[3];
+            Tasks[0] = Task_1;
+            Tasks[1] = Task_2;
+            Tasks[2] = Task_3;
+
+            Parallel.WaitAll(Tasks);
+            Console.WriteLine("Main ended");
+            Console.ReadLine();
+        }
+
+        public static void Task_1()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Thread:" + Thread.CurrentThread.ManagedThreadId + "   " + i);
+            }
+        }
+
+        public static void Task_2()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Thread:" + Thread.CurrentThread.ManagedThreadId + "   " + i);
+            }
+        }
+
+        public static void Task_3()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Thread:" + Thread.CurrentThread.ManagedThreadId + "   " + i);
+            }
+        }
+    }
+}
