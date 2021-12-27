a46a97da128cbdd4fa7d6176e3b6f6d11b393d3f
yurchenkonatalya <70762884+yurchenkonatalya@users.noreply.github.com>
2021-12-27 23:01:05 +0300

    Add files via upload

---
+++ SPP_6/SPP_6/LogBuffer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Concurrent;
+using System.Threading;
+using System.IO;
+namespace SPP_6
+{
+    class LogBuffer
+    {
+        private ConcurrentQueue<string> MagazineMessage;
+        private ConcurrentQueue<string> Buffer;
+        private int LimitBuffer;
+        private System.Timers.Timer Timer;
+        private StreamWriter StreamWriter;
+
+        public LogBuffer(int LimitBuffer, int LimitTimer, string FilePath)
+        {
+            this.LimitBuffer = LimitBuffer;
+            StreamWriter = new StreamWriter(FilePath, true);
+            MagazineMessage = new ConcurrentQueue<string>();
+            Buffer = new ConcurrentQueue<string>();
+            Timer = new System.Timers.Timer(LimitTimer);
+            Timer.Elapsed += TimerActivity;
+            Timer.AutoReset = true;
+            Timer.Enabled = true;
+        }
+
+        public void Add(string Item)
+        {
+            MagazineMessage.Enqueue(Item);
+            Buffer.Enqueue(Item);
+            if (Buffer.Count >= LimitBuffer)
+            {
+                SaveMessage();
+            }
+        }
+
+        private void SaveMessage()
+        {
+            while (Buffer.TryDequeue(out var message))
+            {
+                StreamWriter.WriteLineAsync(message);
+            }
+        }
+
+        private void TimerActivity(object obj, System.Timers.ElapsedEventArgs e)
+        {
+            if (Buffer.Count != 0)
+            {
+                SaveMessage();
+            }
+        }
+        public void Close()
+        {
+            StreamWriter.Close();
+        }
+    }
+}
---
+++ SPP_6/SPP_6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SPP_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogBuffer logBuffer = new LogBuffer(5, 2000, "/Users/yurchenkonatalya/Projects/SPP_6/file.txt");
+
+            for (int i = 0; i < 200; i++)
+            {
+                logBuffer.Add(i.ToString());
+            }
+            logBuffer.Close();
+            Console.WriteLine("Готово");
+            Console.ReadLine();
+        }
+    }
+}
