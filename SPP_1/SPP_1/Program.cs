0ad2f6fe562fc0fab95d612687a6f9220a65a10d
yurchenkonatalya <70762884+yurchenkonatalya@users.noreply.github.com>
2021-12-27 22:55:37 +0300

    Add files via upload

---
+++ SPP_1/SPP_1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+
+namespace SPP_1
+{
+    class Program
+    {
+        private static int numberTreads = 4;
+        static void Main(string[] args)
+        {
+            TaskQueue task = new TaskQueue(numberTreads);
+
+            task.EnqueueTask(CreateDelegate("1"));
+            task.EnqueueTask(CreateDelegate("2"));
+            task.EnqueueTask(CreateDelegate("3"));
+            task.EnqueueTask(CreateDelegate("4"));
+            task.EnqueueTask(CreateDelegate("5"));
+        }
+
+        private static TaskQueue.TaskDelegate CreateDelegate(String str)
+        {
+            return () =>
+            {
+                Console.WriteLine(str);
+                Thread.SpinWait(5000);
+            };
+        }
+    }
+}
---
+++ SPP_1/SPP_1/TaskQueue.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+namespace SPP_1
+{
+    public class TaskQueue
+    {
+        Queue<TaskDelegate> queueTasks = new Queue<TaskDelegate>();
+        public delegate void TaskDelegate();
+
+        public TaskQueue (int n)
+        {
+            Thread[] threads = new Thread[n];
+            while(n != 0)
+            {
+                threads[n - 1] = new Thread(Method);
+                threads[n - 1].Start();
+                n--;
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            lock (queueTasks)
+            {
+                queueTasks.Enqueue(task);
+            }
+        }
+
+        private void Method()
+        {
+
+            while (true)
+            {
+                TaskDelegate task = null;
+                lock (queueTasks)
+                {
+                    if (queueTasks.Count != 0)
+                    {
+                        task = queueTasks.Dequeue();
+                    }
+                }
+                if (task != null)
+                {
+                    task();
+                }
+            }
+        }
+    }
+}
