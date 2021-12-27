9dfd65418143069a95208f90153e08f6e6432b28
yurchenkonatalya <70762884+yurchenkonatalya@users.noreply.github.com>
2021-12-27 23:02:27 +0300

    Add files via upload

---
+++ SPP_8/SPP_8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+///Users/yurchenkonatalya/Projects/SPP_8/SPP_8/bin/Debug/netcoreapp3.1/SPP_8.dll
+namespace SPP_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            string path = Console.ReadLine();
+            Assembly assembly = Assembly.LoadFrom(path);
+            var types = assembly.GetTypes().Where(t => t.IsPublic && t.IsDefined(typeof(ExportClass), false));
+            Console.WriteLine("ExportClass:");
+            foreach (var type in types)
+            {
+                Console.WriteLine(type.FullName);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ExportClass : Attribute { }
+
+    [ExportClass]
+    public class PublicClass { }
+    [ExportClass]
+    internal class InternalClass { }
+}
